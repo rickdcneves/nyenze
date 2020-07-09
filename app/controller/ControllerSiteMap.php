b8698d99db03bf4d4b4091960c40e6b77048976f
Ricardo <ricardodcmeloneves@gmail.com>
2020-07-09 15:17:32 +0100

    MVC config

---
+++ app/controller/ControllerSiteMap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class ControllerSiteMap{
+    
+    public function teste($par1,$par2) {
+        echo "wasa".$par1."---".$par2;
+        
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../config/config.php';
+require_once '../src/vendor/autoload.php';
+use Src\Classes\ClassRoutes;
+
+$d=new App\Dispatch();
+
