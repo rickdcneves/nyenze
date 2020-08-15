70064b6b7197d1c6801f329c60273e0aea7c7833
Ricardo Neves <ricardodcmeloneves@gmail.com>
2020-08-15 06:51:40 +0100

    Nova BD Backup

--- db/nyenze.sql
+++ db/nyenze.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Jul-2020 às 18:27
+-- Tempo de geração: 15-Ago-2020 às 07:50
 -- Versão do servidor: 10.4.13-MariaDB
 -- versão do PHP: 7.4.8
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `artista` (
   `id` int(11) NOT NULL,
+  `pseudonimo` varchar(255) DEFAULT NULL,
   `sobre` mediumtext DEFAULT NULL,
   `ibam` varchar(22) DEFAULT NULL,
   `id_user` int(11) NOT NULL
@@ -38,9 +39,11 @@ CREATE TABLE `artista` (
 -- Extraindo dados da tabela `artista`
 --
 
-INSERT INTO `artista` (`id`, `sobre`, `ibam`, `id_user`) VALUES
-(1, '', '', 6),
-(2, NULL, NULL, 7);
+INSERT INTO `artista` (`id`, `pseudonimo`, `sobre`, `ibam`, `id_user`) VALUES
+(1, 'Nesma123', 'teste', '111111111111111111111', 6),
+(2, 'Za Ca Shi', 'Test1', '999999999999999999999', 7),
+(3, 'Xazama', 'da', '222222222222222222222', 9),
+(4, 'Za Ca Shi', 'Testes', '999999999999999999999', 15);
 
 -- --------------------------------------------------------
 
@@ -78,14 +81,18 @@ CREATE TABLE `contacto` (
 --
 
 INSERT INTO `contacto` (`id`, `tipo`, `contacto`, `id_user`) VALUES
-(1, 'email', 'ricardodcmeloneves@gmail.com', 4),
-(2, 'telefone', '947553985', 4),
 (5, 'email', 'nesma@gmail.com', 6),
 (6, 'telefone', '913136961', 6),
 (7, 'email', 'joao@gmail.com', 7),
 (8, 'telefone', '988888888', 7),
 (9, 'email', 'axel@gmail.com', 8),
-(10, 'telefone', '908876123', 8);
+(10, 'telefone', '908876123', 8),
+(11, 'email', 'xama@gmail.com', 9),
+(12, 'telefone', '929929929', 9),
+(21, 'email', 'ricardodcmeloneves@gmail.com', 14),
+(22, 'telefone', '947553985', 14),
+(23, 'email', 'ricardojorgedacostameloneves@outlook.com', 15),
+(24, 'telefone', '998853924', 15);
 
 -- --------------------------------------------------------
 
@@ -158,7 +165,10 @@ CREATE TABLE `obra` (
 --
 
 INSERT INTO `obra` (`id`, `foto`, `descricao`, `preco`, `estado`, `qtd`, `titulo`, `entrega`, `altura`, `largura`, `id_categoria`, `data_publicacao`, `id_artista`) VALUES
-(1, '1595161949.jpg', 'Joia da coroa cultural angolana', '2000', 1, 2, 'Pensador', 'Disponivel', '15', '8', 1, '19/Jul/20', 1);
+(1, '1595161949.jpg', 'Joia da coroa cultural angolana', '2000', 1, 0, 'Pensador', 'Disponivel', '15', '8', 1, '19/Jul/20', 1),
+(6, '1595704964.jpg', 'Bakama figura de cabinda', '1000', 1, 1, 'Bakama', 'Indisponivel', '21', '12', 1, '25/Jul/20', 1),
+(14, '1596031030.jpeg', 'aaa', '1', 1, 1, 'teste', 'Indisponivel', '1', '1', 1, '29/Jul/20', 2),
+(15, '1597300135.jpg', 'Esta mascara representa um grupo etnico de cabinda', '121', 1, 0, 'Mascara Bakama', 'Indisponivel', '1', '1', 1, '13/Aug/20', 4);
 
 -- --------------------------------------------------------
 
@@ -193,18 +203,21 @@ CREATE TABLE `user` (
   `tipo` varchar(50) NOT NULL,
   `id_morada` int(11) NOT NULL,
   `id_provincia` int(11) NOT NULL,
-  `descricaolocalizacao` mediumtext NOT NULL
+  `descricaolocalizacao` mediumtext NOT NULL,
+  `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `user`
 --
 
-INSERT INTO `user` (`id`, `nome`, `genero`, `dnasc`, `pass`, `tipo`, `id_morada`, `id_provincia`, `descricaolocalizacao`) VALUES
-(4, 'Ricardo Jorge da Costa Melo Neves', 'M', '1999-12-08', 'e5d8ad9ec4447e17d83f2f04a2d95b4a12ed85e9', 'N/Artista', 4, 1, 'Eco-campo, Rua da Policia, Casa S/N'),
-(6, 'Nesma Neves', 'F', '2020-07-16', 'd58531e6ad9b7ffb0c678a6db56bd37e94665b36', 'Artista', 4, 1, 'Centralidade do Sequele'),
-(7, 'João Ricardo', 'M', '2020-07-16', '7fecacc41d35e803c6837ad13c7591920ff330eb', 'Artista', 3, 1, 'Kifica'),
-(8, 'Axel Norman', 'M', '1998-02-12', '8727b15f00705ed99e8dd198428087f0e5b44d41', 'Admin', 3, 1, 'Centralidade do Kilamba');
+INSERT INTO `user` (`id`, `nome`, `genero`, `dnasc`, `pass`, `tipo`, `id_morada`, `id_provincia`, `descricaolocalizacao`, `estado`) VALUES
+(6, 'Nesma Neves', 'F', '2020-07-16', 'd58531e6ad9b7ffb0c678a6db56bd37e94665b36', 'Artista', 4, 1, 'Centralidade do Sequele', 0),
+(7, 'João Ricardo', 'M', '2020-07-16', '7fecacc41d35e803c6837ad13c7591920ff330eb', 'Artista', 3, 1, 'Kifica', 0),
+(8, 'Axel Norman', 'M', '1998-02-12', '8727b15f00705ed99e8dd198428087f0e5b44d41', 'Admin', 3, 1, 'Centralidade do Kilamba', 1),
+(9, 'Xama', 'M', '2020-07-30', '5efaeb0c54b8460068143c81927a65cc97785a03', 'Artista', 3, 1, 'Luanda, Kilamba', 0),
+(14, 'Ricardo Neves', 'M', '2020-08-22', 'e5d8ad9ec4447e17d83f2f04a2d95b4a12ed85e9', 'N/Artista', 3, 1, 'Luanda, Kilamba', 1),
+(15, 'Teste Artista', 'M', '2020-08-13', 'e5d8ad9ec4447e17d83f2f04a2d95b4a12ed85e9', 'Artista', 3, 1, 'Luanda, Kilamba', 1);
 
 -- --------------------------------------------------------
 
@@ -223,8 +236,18 @@ CREATE TABLE `venda` (
   `qtd` int(11) NOT NULL,
   `subtotal` decimal(10,0) NOT NULL,
   `total` decimal(10,0) NOT NULL,
-  `lucro` decimal(10,0) NOT NULL
+  `lucro` decimal(10,0) NOT NULL,
+  `referencia` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `venda`
+--
+
+INSERT INTO `venda` (`id`, `estado`, `id_artista`, `id_user`, `id_obra`, `data_venda`, `data_estado`, `qtd`, `subtotal`, `total`, `lucro`, `referencia`) VALUES
+(3, 'Pago', 4, 14, 15, '14/Aug/20', '14/Aug/20', 1, '121', '151', '30', NULL),
+(4, 'Pago', 1, 14, 1, '14/Aug/20', '14/Aug/20', 1, '2000', '2500', '500', NULL),
+(5, 'Não Confirmado', 1, 14, 1, '14/Aug/20', '14/Aug/20', 1, '2000', '2500', '500', NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -296,7 +319,7 @@ ALTER TABLE `venda`
 -- AUTO_INCREMENT de tabela `artista`
 --
 ALTER TABLE `artista`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `categoria`
@@ -308,7 +331,7 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de tabela `contacto`
 --
 ALTER TABLE `contacto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de tabela `morada`
@@ -320,7 +343,7 @@ ALTER TABLE `morada`
 -- AUTO_INCREMENT de tabela `obra`
 --
 ALTER TABLE `obra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `provincia`
@@ -332,13 +355,13 @@ ALTER TABLE `provincia`
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `venda`
 --
 ALTER TABLE `venda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para despejos de tabelas
